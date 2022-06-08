0331eb12e5e73bfea59a9287bd12208a7df4e2df
oh980225 <oh980225@gmail.com>
2022-06-08 23:15:15 +0900

    test: Test Regitrant => professor case, fail case

--- src/test/java/com/hexagonal/courseregistration/user/RegistrantTest.java
+++ src/test/java/com/hexagonal/courseregistration/user/RegistrantTest.java
@@ -2,7 +2,9 @@ package com.hexagonal.courseregistration.user;
 
 import org.junit.jupiter.api.Test;
 
+import static com.hexagonal.courseregistration.user.Authority.PROFESSOR;
 import static com.hexagonal.courseregistration.user.Authority.STUDENT;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.*;
 
@@ -19,5 +21,28 @@ class RegistrantTest {
     inOrder.verify(allUser, times(1)).exist("17101223", STUDENT);
     inOrder.verify(allUser, times(1)).register("SeungjaeOh", "17101223", STUDENT);
     verifyNoMoreInteractions(allUser);
+  }
+
+  @Test
+  void register_professor() {
+    var allUser = mock(AllUser.class);
+    var registrant = new Registrant(allUser);
+    given(allUser.exist("221103", PROFESSOR)).willReturn(false);
+
+    registrant.register("chulsuKim", "221103", PROFESSOR);
+
+    var inOrder = inOrder(allUser);
+    inOrder.verify(allUser, times(1)).exist("221103", PROFESSOR);
+    inOrder.verify(allUser, times(1)).register("chulsuKim", "221103", PROFESSOR);
+    verifyNoMoreInteractions(allUser);
+  }
+
+  @Test
+  void register_exist_user() {
+    var allUser = mock(AllUser.class);
+    var registrant = new Registrant(allUser);
+    given(allUser.exist("17101223", STUDENT)).willReturn(true);
+
+    assertThrows(ExistUserException.class, () -> registrant.register("SeungjaeOh", "17101223", STUDENT));
   }
 }
